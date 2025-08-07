2c279795fe4e2a239fd11b09490f95ca86ba30a8
KuanningLU <411630097@o365.tku.edu.tw>
2025-08-07 21:48:37 +0800

    調整樣式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,52 @@
 import React from "react";
 import "./Home.css";
 import bearImage from "../assets/bear.png";
+import pencilIcon from "../assets/pencil.png";
+import profileIcon from "../assets/profile.png";
+import bellIcon from "../assets/icon-bell.png";
+import gearIcon from "../assets/icon-gear.png";
+import userIcon from "../assets/icon-user.png";
+import chevronIcon from "../assets/icon-chevron.png";
+import learningIcon from "../assets/icon-learning.png";
 
 const Home: React.FC = () => {
   return (
     <div className="page-wrapper">
-      {/* 頂部標題 */}
+      {/* Header */}
       <header className="site-header">
         <div className="header-left">
+          <img src={profileIcon} alt="Profile" className="profile-icon" />
           <h1 className="greeting-title">阿琪哩賀！</h1>
         </div>
         <nav className="main-navigation">
-          <ul>
-            <li><a href="#learning-mode">學習模式</a></li>
-            <li><a href="#notifications">通知提醒</a></li>
-            <li><a href="#settings">設定</a></li>
-            <li><a href="#login">登入</a></li>
-          </ul>
+          <button className="nav-button">
+            <img src={learningIcon} alt="學習模式" className="nav-icon" />
+            <span className="nav-label">學習模式</span>
+            <img src={chevronIcon} alt="Chevron" className="chevron-icon" />
+          </button>
+          <button className="nav-button">
+            <img src={bellIcon} alt="通知" className="nav-icon" />
+            <span className="nav-label">通知提醒</span>
+          </button>
+          <button className="nav-button">
+            <img src={gearIcon} alt="設定" className="nav-icon" />
+            <span className="nav-label">設定</span>
+          </button>
+          <button className="nav-button">
+            <img src={userIcon} alt="登入" className="nav-icon" />
+            <span className="nav-label">登入</span>
+            <img src={chevronIcon} alt="Chevron" className="chevron-icon" />
+          </button>
         </nav>
       </header>
 
-      {/* 主內容 */}
+      {/* Main Content */}
       <main className="main-content">
-        {/* 左邊大熊 */}
         <div className="character-area">
           <img src={bearImage} alt="小熊" className="character-image" />
           <p className="character-caption">點選小熊開始聊天</p>
         </div>
 
-        {/* 右邊聊天區 */}
         <div className="chat-area">
           <div className="chat-log">
             <div className="message-group incoming">
@@ -45,6 +63,7 @@ const Home: React.FC = () => {
           </div>
 
           <form className="chat-input-form">
+            <img src={pencilIcon} alt="pencil" className="input-icon" />
             <input type="text" placeholder="請輸入..." />
           </form>
         </div>
